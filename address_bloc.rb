864ca745d7ae6999e3bd1e4f76f7359250bf09df
Jim Knopf <jsk4@mac.com>
2018-02-14 11:07:05 -0800

    Checkpoint 5: Address Bloc:Menu completed

--- address_bloc.rb
+++ address_bloc.rb
@@ -1,1 +1,9 @@
+require_relative 'controllers/menu_controller'
+
+
+menu = MenuController.new   #  create a new MenuController when AddressBloc starts
+
+system "clear"    #clear command line
 puts "Welcome to AddressBloc!"
+
+menu.main_menu     #call main_menu to display menu
