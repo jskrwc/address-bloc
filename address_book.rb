3faa0cd8f81fdc9995b3f9a95b14ca09c36cb1b4
Jim K <jsk4@mac.com>
2018-02-13 16:37:33 -0800

    Add files via upload
    re-uploading the address_book file with remove_entry functionality

--- address_book.rb
+++ address_book.rb
@@ -1,23 +1,37 @@
 require_relative 'entry'
 
-  class AddressBook
-    attr_reader :entries
+class AddressBook
+  attr_reader :entries
 
-    def initialize
-      @entries = []
-    end
-
-    def add_entry(name, phone_number, email)
-      # #9
-      index = 0
-      entries.each do |entry|
-      # #10
-        if name < entry.name
-          break
-        end
-        index+= 1
-      end
-      # #11
-      entries.insert(index, Entry.new(name, phone_number, email))
-    end
+  def initialize
+    @entries = []
   end
+
+  def add_entry(name, phone_number, email)
+    # #9
+    index = 0
+    @entries.each do |entry|
+    # #10
+      if name < entry.name
+        break
+      end
+      index+= 1
+    end
+    # #11
+    @entries.insert(index, Entry.new(name, phone_number, email))
+  end
+
+  def remove_entry(name, phone_number, email)
+
+    entry_to_remove = nil
+
+    @entries.each do |entry|
+      if entry.name == name && entry.phone_number == phone_number && entry.email == email
+        entry_to_remove = entry
+      end
+    end
+
+    @entries.delete(entry_to_remove)
+  end
+
+end
