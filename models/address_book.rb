f907f181a1b45cff3bd4700382d13e4415171d39
Jim Knopf <jsk4@mac.com>
2018-02-14 17:05:51 -0800

    Checkpoint 6: Reading CSVs completed

--- models/address_book.rb
+++ models/address_book.rb
@@ -1,4 +1,5 @@
 require_relative 'entry'
+require "csv"
 
 class AddressBook
   attr_reader :entries
@@ -21,6 +22,7 @@ class AddressBook
     @entries.insert(index, Entry.new(name, phone_number, email))
   end
 
+  # Assignment 4
   def remove_entry(name, phone_number, email)
 
     entry_to_remove = nil
@@ -34,4 +36,16 @@ class AddressBook
     @entries.delete(entry_to_remove)
   end
 
+  # Checkpoint 6
+  def import_from_csv(file_name)
+    # read file, use CSV class to parse-- results in object of type CSV::Table
+    csv_text = File.read(file_name)
+    csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+    # iterate over teh CSV::Table object's rows
+    # convert each row_hash to and Entry using add_entry method
+    csv.each do |row|
+      row_hash = row.to_hash
+      add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+    end
+  end
 end
